71c00a5cb405952d374d0eeae16d76240fd012df
Shyplants <jinuchae9242@gmail.com>
2023-01-23 19:24:30 +0900

    Enter 또는 ArrowDown 키 입력시 nextBtn 클릭, ArrowUp 키 입력시 prevBtn 클릭 함수호출

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,6 @@
+let currentIndex = 0;
+let wordCnt = 0;
+
 function setCurrentIndex(index) {
   document.querySelector('#current').innerText = index;
 }
@@ -6,13 +9,45 @@ function setTotalWordSize(amt) {
   document.querySelector('#total').innerText = amt;
 }
 
+function doKeyPress(e) {
+  if(e.key == 'Enter' || e.key == 'ArrowDown') {
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {action: 'nextBtn'}, (response) => {
+
+        if(response.status == 0) {
+          currentIndex = parseInt(response.currentIndex);
+
+          if(wordCnt > 0)
+            currentIndex++; 
+          setCurrentIndex(currentIndex);
+        }
+      });
+    });
+  }
+  else if(e.key == 'ArrowUp') {
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {action: 'prevBtn'}, (response) => {
+
+        if(response.status == 0) {
+          currentIndex = parseInt(response.currentIndex);
+
+          if(wordCnt > 0)
+            currentIndex++; 
+          setCurrentIndex(currentIndex);
+        }
+      });
+    });
+  }
+}
+
 function init() {
   let serachInput = document.querySelector('.searchBarInput');
   let prevBtn = document.querySelector('#prevBtn');
   let nextBtn = document.querySelector('#nextBtn');
+  let closeBtn = document.querySelector('#closeBtn');
 
-  let currentIndex = 0;
-  let wordCnt = 0;
+  currentIndex = 0;
+  wordCnt = 0;
 
   serachInput.addEventListener('change', ()=> {
     chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
@@ -65,6 +100,12 @@ function init() {
         }
       });
     });
+  });
+
+  window.addEventListener('keydown', doKeyPress, false);
+
+  closeBtn.addEventListener('click', () => {
+
   });
 
 }
